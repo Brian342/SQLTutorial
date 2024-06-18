7c4708737462083b913d01910acd42dcc792a0b5
Brian342 <migelbrian3@gmail.com>
2024-06-18 03:26:24 +0300

    orderby

--- GroupByAndOrderBy.sql
+++ GroupByAndOrderBy.sql
@@ -1,10 +1,16 @@
 /* Group By Order By*/
 
+-- SELECT *
+-- FROM EmployeeDemographics
+-- ORDER BY Age DESC ,Gender DESC
+
 SELECT *
 FROM EmployeeDemographics
+ORDER BY 4 DESC ,5 DESC --4 represent the column 4 and the 5 represent the column 5
 
 
-
-SELECT Gender, Age, COUNT(Gender)
-FROM EmployeeDemographics
-GROUP BY Gender,Age
+-- SELECT Gender, Age, COUNT(Gender) AS CountGender
+-- FROM EmployeeDemographics
+-- WHERE Age > 31
+-- GROUP BY Gender,Age
+-- ORDER BY CountGender --used to arrange elements in asc or desc order
