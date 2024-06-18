09e6bd7971d0aa1a7f0cc3b9dea0d4a82edf5984
Brian342 <migelbrian3@gmail.com>
2024-06-19 01:47:32 +0300

    case statement

---
+++ CaseStatement.sql
@@ -0,0 +1,7 @@
+/*Case Statement - allows one to specify condition and 
+allows what should be retured if the condition is meet*/
+
+SELECT FirstName,LastName,Age
+FROM SqlTutorial.dbo.EmployeeDemographics
+WHERE Age is NOT NULL
+
