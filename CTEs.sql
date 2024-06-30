fc5c972e6af72e97d4404a5de1d231f7d3da59d5
Brian342 <migelbrian3@gmail.com>
2024-06-30 21:51:53 +0300

    common Table Expression

--- CTEs.sql
+++ CTEs.sql
@@ -1,1 +1,12 @@
 /*CTEs - Common Table Expressions used to manipulate complex data*/
+WITH CTE_Employee as 
+(SELECT FirstName, LastName, Gender, Salary
+, COUNT(Gender) OVER (PARTITION BY Gender) as TotalGender
+, AVG(Salary) OVER (PARTITION BY Gender) as AVGSalary
+FROM SqlTutorial..EmployeeDemographics dem
+JOIN SqlTutorial..EmployeSalary sal
+    ON dem.EmployeeID = sal.EmployeeID
+WHERE Salary > '45000'
+)
+SELECT FirstName, AVGSalary
+FROM CTE_Employee
