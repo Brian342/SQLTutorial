4428c0bcc38d5fe4f09cd95b88c4dbb395848e4e
Brian342 <migelbrian3@gmail.com>
2024-06-30 21:58:31 +0300

    temptableshow they work

--- TemporaryTables.sql
+++ TemporaryTables.sql
@@ -1,1 +1,19 @@
 /* Temp Tables */
+
+CREATE Table #temp_employee (
+    EmployeeID int,
+    JobTitle VARCHAR(100),
+    Salary int
+)
+
+SELECT *
+FROM #temp_employee
+
+INSERT INTO #temp_employee VALUEs (
+    '1001', 'HR', '45000'
+
+)
+
+INSERT INTO #temp_employee
+select *
+FROM SqlTutorial..employesalary
