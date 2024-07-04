41327d1656ad96147025d24c766099cf15fd6696
Brian342 <migelbrian3@gmail.com>
2024-07-04 15:16:24 +0300

    trim

--- String_function.sql
+++ String_function.sql
@@ -17,3 +17,17 @@ INSERT into EmployeeErrors VALUES
 SELECT *
 FROM EmployeeErrors
 
+-- Using Trim, Ltrim, Rtrim
+
+SELECT EmployeeID, TRIM(EmployeeID) as IDTrim -- generally removes spaces 
+FROM EmployeeErrors 
+
+--Ltrim
+
+SELECT EmployeeID, LTRIM(EmployeeID) as IDRTrim --removes spaces on the left side
+FROM EmployeeErrors 
+
+--Rtrim
+
+SELECT EmployeeID, RTRIM(EmployeeID) as IDLTriM -- Removes spaces on the right side
+FROM EmployeeErrors 
