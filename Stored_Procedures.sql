c58e148a0f2644291b2874e8a8468d9c0fa6bf7d
Brian342 <migelbrian3@gmail.com>
2024-07-04 16:57:28 +0300

    sql sucks

---
+++ Stored_Procedures.sql
@@ -0,0 +1,30 @@
+/* Stored procedures- a group of sql statements 
+being created and stored in a database */
+
+-- CREATE PROCEDURE TEST
+-- AS
+-- SELECT *
+-- FROM EmployeeDemographics
+
+-- EXEC TEST
+
+CREATE PROCEDURE Temp_Employee
+AS
+CREATE table #temp_employee (
+    JobTitle VARCHAR(100),
+    EmployeesPerJob int,
+    AvgAge int,
+    AvgSalary int
+) 
+
+Insert into #temp_employee
+SELECT JobTitle,COUNT(JobTitle), AVG(age), AVG(Salary)
+FROM SqlTutorial..EmployeeDemographics emp
+JOIN SqlTutorial..EmployeSalary sal
+     ON emp.EmployeeID = sal.EmployeeID
+group by JobTitle
+
+SELECT *
+FROM #temp_employee
+
+EXEC Temp_Employee 
