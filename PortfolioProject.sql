43cbcdd702d8a7b14798cc980f8c3922737918f6
Brian342 <migelbrian3@gmail.com>
2024-09-11 01:51:41 +0300

    sql data manipulation

--- PortfolioProject.sql
+++ PortfolioProject.sql
@@ -1,5 +1,6 @@
 SELECT *
 FROM Portfolio_project..CovidDeaths
+where continent is not null
 ORDER BY 3,4
 
 
@@ -10,21 +11,38 @@ ORDER BY 3,4
 -- select Data that we are going to be using
 SELECT location, date, total_cases, new_cases, total_deaths, population 
 FROM Portfolio_project..CovidDeaths
+where continent is not null
 ORDER BY 1,2
 
 -- Looking at total cases vs Total Deaths
 -- Shows likelihood of dying if you contract covid in your country
 SELECT location, date, total_cases, total_deaths, (CAST(total_deaths AS FLOAT)/total_cases)*100 as DeathPercentage
 FROM Portfolio_project..CovidDeaths
-where location like '%kenya%'
+where location like '%kenya%' and continent is not null
 ORDER BY 1,2
 
 -- looking at total cases vs population
 -- shows what percentage of popluation got covid
-SELECT location, date, population, total_cases, (CAST(total_cases AS FLOAT)/population)*100 as DeathPercentage
+SELECT location, date, population, total_cases, (CAST(total_cases AS FLOAT)/population)*100 as Percentageofpopulation
 FROM Portfolio_project..CovidDeaths
-where location like '%kenya%'
+where continent is not null
+--where location like '%kenya%'
 ORDER BY 1,2
 
+-- Looking at countries with highest infection rate compared to population
+SELECT location, population, MAX(total_cases) AS HighestInfectionCount, MAX(CAST(total_cases AS FLOAT)/population)*100 as Percentageofpopulationinfected
+FROM Portfolio_project..CovidDeaths
+--where location like '%kenya%'
+where continent is not null
+GROUP BY location, population
+ORDER BY Percentageofpopulationinfected DESC
+
+--Showing countries with highest Death count per population
+SELECT location, MAX(CAST(total_deaths AS INT)) AS TotalDeathCount
+FROM Portfolio_project..CovidDeaths
+--where location like '%kenya%'
+where continent is not null
+GROUP BY location
+ORDER BY TotalDeathCount DESC
 
 
