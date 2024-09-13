78c67f9d72459c5ec54703e28ece72e215d61640
Brian342 <migelbrian3@gmail.com>
2024-09-13 20:27:32 +0300

    data cleaning using sql

--- Datacleaning.sql
+++ Datacleaning.sql
@@ -19,4 +19,24 @@ SET SaleDateConverted = CONVERT(Date,SaleDate)
 -- Populate property Address Data
 SELECT *
 FROM Portfolio_project.dbo.NashvilleHousing
-WHERE PropertyAddress is NULL
+--WHERE PropertyAddress is NULL
+order by ParcelID
+
+SELECT a.ParcelID, a.PropertyAddress, b.ParcelID, b.PropertyAddress, ISNULL(a.PropertyAddress, b.PropertyAddress)
+FROM Portfolio_project.dbo.NashvilleHousing a
+join Portfolio_project.dbo.NashvilleHousing b
+    on a.ParcelID = b.ParcelID
+    and a.[UniqueID] <> b.[UniqueID]
+where a.PropertyAddress is null
+
+UPDATE a
+SET PropertyAddress = ISNULL(a.PropertyAddress, b.PropertyAddress)
+FROM Portfolio_project.dbo.NashvilleHousing a
+join Portfolio_project.dbo.NashvilleHousing b
+    on a.ParcelID = b.ParcelID
+    and a.[UniqueID] <> b.[UniqueID]
+where a.PropertyAddress is null
+
+-- Breaking out Address into Individual columns (Address, City, State)
+SELECT PropertyAddress
+FROM Portfolio_project.dbo.NashvilleHousing
