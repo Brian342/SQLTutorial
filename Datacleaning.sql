4643c851b4ef0f9f6cecbdcee2237b673a21c59b
Brian342 <migelbrian3@gmail.com>
2024-09-13 22:49:23 +0300

    Data cleaning in sql

--- Datacleaning.sql
+++ Datacleaning.sql
@@ -40,3 +40,60 @@ where a.PropertyAddress is null
 -- Breaking out Address into Individual columns (Address, City, State)
 SELECT PropertyAddress
 FROM Portfolio_project.dbo.NashvilleHousing
+
+SELECT 
+SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress) -1) as Address
+, SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress)+ 1, LEN(PropertyAddress)) as Address
+FROM Portfolio_project.dbo.NashvilleHousing
+
+ALTER TABLE NashvilleHousing
+ADD PropertySplitAddress NVARCHAR(255);
+
+update NashvilleHousing
+SET PropertySplitAddress = SUBSTRING(PropertyAddress, 1, CHARINDEX(',',PropertyAddress) -1)
+
+ALTER TABLE NashvilleHousing
+ADD PropertySplitCity NVARCHAR(255);
+
+update NashVilleHousing
+set PropertySplitCity = SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) + 1, LEN(PropertyAddress))
+
+
+SELECT *
+FROM  Portfolio_project.dbo.NashvilleHousing
+
+
+SELECT OwnerAddress
+FROM  Portfolio_project.dbo.NashvilleHousing
+
+
+select 
+PARSENAME(Replace(OwnerAddress, ',','.'),3)
+, PARSENAME(Replace(OwnerAddress, ',','.'),2)
+, PARSENAME(Replace(OwnerAddress, ',','.'),1)
+from Portfolio_project.dbo.NashvilleHousing
+
+
+ALTER TABLE NashvilleHousing
+ADD OwnerSplitAddress NVARCHAR(255);
+
+update NashvilleHousing
+SET OwnerSplitAddress = PARSENAME(Replace(OwnerAddress, ',','.'),3)
+
+
+ALTER TABLE NashvilleHousing
+ADD OwnerSplitCity NVARCHAR(255);
+
+update NashVilleHousing
+set OwnerSplitCity = PARSENAME(Replace(OwnerAddress, ',','.'),2)
+
+
+ALTER TABLE NashvilleHousing
+ADD OwnerSplitState NVARCHAR(255);
+
+update NashVilleHousing
+set OwnerSplitState = PARSENAME(Replace(OwnerAddress, ',','.'),1)
+
+
+SELECT *
+FROM  Portfolio_project.dbo.NashvilleHousing
