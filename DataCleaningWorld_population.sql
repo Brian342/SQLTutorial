35f281ebdb6073993b9cf7db8b9366b2dcf4e15d
Brian kimanzi <migelbrian3@gmail.com>
2024-11-25 04:55:01 +0300

    data cleaning in sql

--- DataCleaningWorld_population.sql
+++ DataCleaningWorld_population.sql
@@ -15,3 +15,32 @@ SELECT Capital
 FROM world_population
 ORDER BY [Rank]
 ;
+
+-- Removing speical characters on the Capital column
+GO -- Start of the removing special char on data set and also end of the select scrip
+CREATE FUNCTION dbo.removecharonCapital(@capitalInput NVARCHAR(MAX))
+RETURNS NVARCHAR(Max)
+AS
+BEGIN
+        DECLARE @capitalOutput NVARCHAR(MAX) = '';
+        DECLARE @i INT = 1;
+        DECLARE @char NCHAR(1);
+
+        WHILE @i <= LEN(@capitalInput)
+        BEGIN
+            SET @char = SUBSTRING(@capitalInput, @i, 1);
+            IF @char LIKE '[a-zA-Z0-9]'
+                SET @capitalOutput = @capitalOutput + @char;
+            ELSE
+                SET @capitalOutput = @capitalOutput + '';
+            SET @i = @i + 1;
+        END
+        RETURN LTRIM(RTRIM(@capitalOutput));
+END;
+
+GO -- end of the function
+
+UPDATE world_population
+
+
+
