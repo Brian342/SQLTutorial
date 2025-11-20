06f8bdde530c60ace9b62a248b20c1e1a321eb12
Brian kimanzi <migelbrian3@gmail.com>
2025-11-20 09:23:17 +0300

    new code update

--- Alx.sql
+++ Alx.sql
@@ -1,5 +1,8 @@
-ALTER TABLE Access_to_basic_services
+-- ALTER TABLE Access_to_basic_services
 -- MODIFY COLUMN Country_name VARCHAR(37); used to modify the column datatype
 -- ADD Gini_index FLOAT; Used to add the coumn name
 -- DROP COLUMN Gini_index; used to drop a specific column
+
+-- DROP TABLE Access_to_basic_services; used to drop the a specific table
+-- DROP DATABASE united_nations; used to drop a database
 
