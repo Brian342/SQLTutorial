0ce182c5406cd84f3f1b9723256dd750206b19c0
Brian kimanzi <migelbrian3@gmail.com>
2026-01-31 08:26:52 +0300

    new code update

---
+++ StringManipulationALX.sql
@@ -0,0 +1,7 @@
+-- cast function used to change one datatype to another
+SELECT DISTINCT
+	Country_name,
+    Time_period,
+    Est_population_in_millions
+FROM united_nations.Access_to_basic_services;
+	
