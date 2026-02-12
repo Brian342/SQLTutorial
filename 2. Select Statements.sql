0aa495f20c59cc9c44457408fa26e61cc1da468e
Brian kimanzi <migelbrian3@gmail.com>
2026-02-12 10:25:58 +0300

    new code update

---
+++ 2. Select Statements.sql
@@ -0,0 +1,15 @@
+SELECT *
+	FROM Parks_and_Recreation.employee_demographics;
+    
+SELECT first_name,
+last_name,
+age,
+age + 10 AS age_added_10_years
+	FROM Parks_and_Recreation.employee_demographics;
+
+# PEMDAS (paranthesis, exponents, multiplication, division, addition, subtration)
+
+SELECT DISTINCT first_name, gender
+	FROM Parks_and_Recreation.employee_demographics;
+    
+    
