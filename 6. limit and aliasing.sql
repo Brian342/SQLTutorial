83c1bd27e64006d9d89c87a2e0b00f9b94f8361f
Brian kimanzi <migelbrian3@gmail.com>
2026-02-13 18:01:05 +0300

    new code update

--- 6. limit and aliasing.sql
+++ 6. limit and aliasing.sql
@@ -10,3 +10,11 @@ SELECT *
     ORDER BY age DESC
     LIMIT 2, 1 -- start at position 2 and return the one after
 ;
+
+-- Aliasing changing name of columns
+
+SELECT gender, AVG(age) AS avg_age
+	FROM Parks_and_Recreation.employee_demographics
+    GROUP BY gender
+    HAVING avg_age > 41
+;
