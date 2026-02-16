e676d35094b0a5e336083686c9b22afb498648a5
Brian kimanzi <migelbrian3@gmail.com>
2026-02-17 00:53:57 +0300

    new code update

--- 9.string functions.sql
+++ 9.string functions.sql
@@ -1,3 +1,21 @@
 -- String functions
 use Parks_and_Recreation
 ;
+-- length
+SELECT length('skyfall')
+;
+
+SELECT first_name, length(first_name) AS Name_Length
+	FROM Employee_demographics
+ORDER BY 2
+;
+
+-- using upper and lower functions
+SELECT upper('sky');
+SELECT lower('SKY');
+
+SELECT first_name, UPPER(first_name) AS caps_name
+	FROM Employee_demographics
+;
+
+-- Trim, LTrim, RTrim
