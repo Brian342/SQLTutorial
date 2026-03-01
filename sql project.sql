7759d8497133da9c26f124869b37b34e60224e98
Brian kimanzi <migelbrian3@gmail.com>
2026-03-01 11:49:09 +0300

    new code update

--- sql project.sql
+++ sql project.sql
@@ -1,2 +1,12 @@
-SELECT * FROM world_layoff.layoffs_staging2;
--- new
+use databasesoccer
+;
+
+SELECT *
+	FROM databasesoccer.directory
+;
+
+-- 1. Remove Duplicates
+-- 2. Standardize the data
+-- 3. Null Values or Blank values
+-- 4. Remove Any columns
+
