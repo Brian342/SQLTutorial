bb43de91bbcd2faf3473cd43612954fe15b900c7
Brian kimanzi <migelbrian3@gmail.com>
2026-03-01 12:03:08 +0300

    new code update

--- sql project.sql
+++ sql project.sql
@@ -5,8 +5,27 @@ SELECT *
 	FROM databasesoccer.directory
 ;
 
+-- Data Cleaning Steps to Follow
 -- 1. Remove Duplicates
 -- 2. Standardize the data
 -- 3. Null Values or Blank values
 -- 4. Remove Any columns
+
+-- Remove Duplicates
+
+# creating a duplicate table to work on the dataset
+CREATE TABLE dup_soccer_table
+LIKE databasesoccer.directory
+;
+
+SELECT *
+	FROM dup_soccer_table
+;
+
+# insert details to the table
+INSERT dup_soccer_table
+SELECT *
+	FROM databasesoccer.directory
+;
+
 
